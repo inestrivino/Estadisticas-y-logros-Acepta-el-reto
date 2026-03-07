56c0785734377b297d6b4ab266e85679247022ae
N3STOR173 <nestorgmayor@gmail.com>
2026-03-07 21:27:31 +0100

    error en el cliente de redis al testear

--- Aplicacion/back/src/redis/redisClient.ts
+++ Aplicacion/back/src/redis/redisClient.ts
@@ -2,6 +2,8 @@ import { createClient } from "redis";
 
 const redisClient = createClient({ url: process.env.REDIS_URL || "redis://localhost:6379" });
 
+export default redisClient;
+
 await redisClient.connect()
   .then(
     () => console.log(" * Conexión establecida con Redis")
@@ -9,5 +11,3 @@ await redisClient.connect()
   .catch(
     err => console.error("No se pudo conectar a Redis", err)
   );
-
-export default redisClient;
