3935837e69acddf850718115999adf6b5f1c8eaa
N3STOR173 <nestorgmayor@gmail.com>
2026-05-18 21:20:32 +0200

    fix sockets

--- Aplicacion/front/src/services/socket.ts
+++ Aplicacion/front/src/services/socket.ts
@@ -1,5 +1,3 @@
 import { io } from "socket.io-client";
 
-const socketUrl = import.meta.env.VITE_SOCKET_URL ?? 'ws://localhost:3000';
-
-export const socket = io(socketUrl);
+export const socket = io();
