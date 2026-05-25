49f97f539775b60d0d795cbfeff6d24e82b8cbbb
N3STOR173 <nestorgmayor@gmail.com>
2026-05-25 03:03:13 +0200

    fuentes self hosted

--- Aplicacion/front/src/main.tsx
+++ Aplicacion/front/src/main.tsx
@@ -1,6 +1,16 @@
 import { createRoot } from "react-dom/client";
 import App from "./app.tsx";
 import { StrictMode } from "react";
+
+import "@fontsource/open-sans/300.css";
+import "@fontsource/open-sans/400.css";
+import "@fontsource/open-sans/600.css";
+import "@fontsource/open-sans/700.css";
+
+import "@fontsource/jetbrains-mono/400.css";
+import "@fontsource/jetbrains-mono/600.css";
+import "@fontsource/jetbrains-mono/700.css";
+
 import './index.css';
 
 const domNode3 = document.getElementById("root");
